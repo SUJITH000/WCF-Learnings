56d642c345bf120abf83876802828548127570cd
SKY_000 <sujithkumary000@gmail.com>
2023-03-05 12:11:53 +0530

    adding files ...!

---
+++ 4th/CompanyClient/CompanyClient/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CompanyClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="table-layout:auto">
+            <asp:Button ID="Button1" runat="server" Text="Get Public Information" OnClick="Button1_Click" />&nbsp;
+            <asp:Label ID="Label1" runat="server" Font-Bold="true"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button2" runat="server" Text="Get Confidential Information" OnClick="Button2_Click" />&nbsp;
+            <asp:Label ID="Label2" runat="server" Font-Bold="true"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 4th/CompanyClient/CompanyClient/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CompanyClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CompanyService.CompanyPublicServiceClient publicServiceClient = new 
+                CompanyService.CompanyPublicServiceClient("BasicHttpBinding_ICompanyPublicService");
+            Label1.Text= publicServiceClient.GetPublicInformation();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            CompanyService.CompanyConfidentialServiceClient confidentialServiceClient = new
+                CompanyService.CompanyConfidentialServiceClient("NetTcpBinding_ICompanyConfidentialService");
+            Label2.Text=confidentialServiceClient.GetConfidentialInformation();
+        }
+    }
+}
